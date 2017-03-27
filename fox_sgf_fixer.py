0aaf53a1e868335c60afd285e75f86fa7f033c9e
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-03-27 02:51:56 +0100

    various improvements

--- fox_sgf_fixer.py
+++ fox_sgf_fixer.py
@@ -3,18 +3,37 @@ import gofish
 
 
 KNOWN_PLAYERS = {
+	"小香馋猫":		"Chang Hao",
+	"谜团":			"Chen Yaoye",
 	"绝艺":			"Fine Art",
 	"星宿老仙":		"Gu Li",
 	"潜伏":			"Ke Jie",
 	"剑过无声":		"Lian Xiao",
+	"愿我能":			"Meng Tailing",
+	"聂卫平":			"Nie Weiping",
+	"段誉":			"Tan Xiao",
+	"天选":			"Tuo Jiaxi",
+	"周俊勳":			"Zhou Junxun",
+
 	"airforce9":	"Kim Jiseok",
+	"black2012":	"Li Qinchong",
 	"Eason":		"Zhou Ruiyang",
 	"INDIANA13":	"Gu Zihao",
+	"jingjing":		"Dang Yifei",
 	"jpgo01":		"Iyama Yuta",
+	"kongm":		"Huang Yusong",
+	"kuangren":		"Jiang Weijie",
 	"leaf":			"Shi Yue",
 	"maker":		"Park Junghwan",
 	"Master":		"AlphaGo",
 	"nparadigm":	"Shin Jinseo",
+	"piaojie":		"Kang Dongyun",
+	"pyh":			"Park Yeonghun",
+	"shadowpow":	"Cho Hanseung",
+	"smile":		"Yang Dingxin",
+	"spinmove":		"An Sungjoon",
+	"wonfun":		"Weon Seongjin",
+	"ykpcx":		"Fan Tingyu",
 }
 
 
@@ -36,20 +55,25 @@ def deal_with_file(filename):
 			if root.properties["HA"] == ["0"]:
 				root.delete_property("HA")
 
+		black_for_filename = root.properties["PB"][0]
+		white_for_filename = root.properties["PW"][0]
+
 		try:
 			black, white, = re.search(r"\[(.+)\]vs\[(.+)\]\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d\d\.sgf", filename).group(1, 2)
 			if black in KNOWN_PLAYERS:
-				root.safe_commit("PB", KNOWN_PLAYERS[black])
+				root.safe_commit("PB", "{} ({})".format(black, KNOWN_PLAYERS[black]))
+				black_for_filename = KNOWN_PLAYERS[black]
 			if white in KNOWN_PLAYERS:
-				root.safe_commit("PW", KNOWN_PLAYERS[white])
+				root.safe_commit("PW", "{} ({})".format(white, KNOWN_PLAYERS[white]))
+				white_for_filename = KNOWN_PLAYERS[white]
 		except:
 			pass
 
-		rp = root.properties
+		dt = root.properties["DT"][0]
 
-		newfilename = "{} {} vs {}.sgf".format(rp["DT"][0], rp["PB"][0], rp["PW"][0])
+		newfilename = "{} {} vs {}.sgf".format(dt, black_for_filename, white_for_filename)
 		if os.path.exists(newfilename):
-			newfilename = "{} {} vs {} ({}).sgf".format(rp["DT"][0], rp["PB"][0], rp["PW"][0], root.dyer().replace("?", "_"))
+			newfilename = "{} {} vs {} ({}).sgf".format(dt, black_for_filename, white_for_filename, root.dyer().replace("?", "_"))
 
 		gofish.save(newfilename, root)
 
